a37c249fa4811b7f0e0559ff3d622d74fc7a357f
al <al@DESKTOP-L1789NP>
2021-11-26 20:08:22 +0500

    Add project files.

---
+++ ParallelForDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParallelForDemo
+{
+    class Program
+    {
+        static int Max = 1;
+        static void Maxnumber(int c)
+        {
+            Random rnd = new Random();
+            int v = rnd.Next(0, c);
+
+            if (Max < v) {
+                Max = v;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+
+            Parallel.For(1, 10000000, i => {
+               Maxnumber(i);
+           });
+
+            long elapsed = sw.ElapsedMilliseconds;
+            Console.WriteLine("Время выполнения алгоритма в миллисекундах: {0}", elapsed);
+
+            Console.WriteLine("Максимальное число: {0} ", Max);
+
+            Console.ReadLine();
+
+        }
+    }
+}
